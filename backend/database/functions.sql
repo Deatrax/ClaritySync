9106e5a2095056a53526c6b611bbb4e59425cd74
Deatrax <161139217+Deatrax@users.noreply.github.com>
2026-01-29 02:56:42 +0600

    overdraft protection

--- backend/database/functions.sql
+++ backend/database/functions.sql
@@ -116,6 +116,16 @@ BEGIN
     -- If Money Going OUT (PAYMENT, TRANSFER) -> Deduct from 'from_account'
     IF NEW.transaction_type::text IN ('PAYMENT', 'TRANSFER') THEN
         IF NEW.from_account_id IS NOT NULL THEN
+            
+            -- Check for sufficient funds
+            PERFORM 1 FROM banking_account 
+            WHERE account_id = NEW.from_account_id 
+            AND current_balance < NEW.amount;
+            
+            IF FOUND THEN
+                RAISE EXCEPTION 'Insufficient funds: Transaction amount % exceeds current balance.', NEW.amount;
+            END IF;
+
             UPDATE banking_account SET current_balance = current_balance - NEW.amount 
             WHERE account_id = NEW.from_account_id;
         END IF;
