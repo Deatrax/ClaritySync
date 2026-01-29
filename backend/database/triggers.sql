7efd470abc8f1ac7b62c7842ed758fcc9b617f04
Deatrax <161139217+Deatrax@users.noreply.github.com>
2026-01-29 09:15:41 +0600

    commented out removed trigger

--- backend/database/triggers.sql
+++ backend/database/triggers.sql
@@ -1,7 +1,7 @@
 CREATE TRIGGER trg_auto_update_balance AFTER INSERT ON public.transaction FOR EACH ROW EXECUTE FUNCTION fn_update_bank_balance();
 
 
-CREATE TRIGGER trg_aftersaleinsert AFTER INSERT ON public.sales FOR EACH ROW EXECUTE FUNCTION fn_after_sale_insert();
+-- REMOVED: CREATE TRIGGER trg_aftersaleinsert AFTER INSERT ON public.sales FOR EACH ROW EXECUTE FUNCTION fn_after_sale_insert();
 
 
 
